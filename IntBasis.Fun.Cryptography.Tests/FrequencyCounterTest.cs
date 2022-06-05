863b1b03b005fe6d32c6a8109369cb8fc4e40ecb
Jacob Foshee <jacobf@gmail.com>
2022-06-05 11:01:35 -0500

    feat(freq): Implement FrequencyCounter

--- IntBasis.Fun.Cryptography.Tests/FrequencyCounterTest.cs
+++ IntBasis.Fun.Cryptography.Tests/FrequencyCounterTest.cs
@@ -1,4 +1,6 @@
 using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace IntBasis.Fun.Cryptography.Tests;
@@ -9,5 +11,95 @@ public class FrequencyCounterTest
     public void Construction(FrequencyCounter frequencyCounter)
     {
         frequencyCounter.Should().NotBeNull();
+    }
+
+    [Theory(DisplayName = "FrequencyCounter: Base Case"), AutoMoq]
+    public void Base(FrequencyCounter frequencyCounter)
+    {
+        var frequencyAnalysis = frequencyCounter.GetFrequencyAnalysis("");
+        frequencyAnalysis.TokenCount.Should().BeEmpty();
+        frequencyAnalysis.TokensByFrequency.Should().BeEmpty();
+    }
+
+    [Theory(DisplayName = "FrequencyCounter: One Token"), AutoMoq]
+    public void OneToken(FrequencyCounter frequencyCounter)
+    {
+        var frequencyAnalysis = frequencyCounter.GetFrequencyAnalysis("a");
+        frequencyAnalysis.TokenCount.Should().Equal(new Dictionary<char, int>
+        {
+            ['a'] = 1
+        });
+        frequencyAnalysis.TokensByFrequency.Should().Equal('a');
+    }
+
+    [Theory(DisplayName = "FrequencyCounter: Three Tokens"), AutoMoq]
+    public void ThreeTokens(FrequencyCounter frequencyCounter)
+    {
+        var frequencyAnalysis = frequencyCounter.GetFrequencyAnalysis("abc");
+        frequencyAnalysis.TokenCount.Should().Equal(new Dictionary<char, int>
+        {
+            ['a'] = 1,
+            ['b'] = 1,
+            ['c'] = 1,
+        });
+        frequencyAnalysis.TokensByFrequency.Should().Equal('a', 'b', 'c');
+    }
+
+    [Theory(DisplayName = "FrequencyCounter: One Repeat Token"), AutoMoq]
+    public void OneRepeatToken(FrequencyCounter frequencyCounter)
+    {
+        var frequencyAnalysis = frequencyCounter.GetFrequencyAnalysis("aa");
+        frequencyAnalysis.TokenCount.Should().Equal(new Dictionary<char, int>
+        {
+            ['a'] = 2
+        });
+        frequencyAnalysis.TokensByFrequency.Should().Equal('a');
+    }
+
+    [Theory(DisplayName = "FrequencyCounter: Multiple Repeat Tokens"), AutoMoq]
+    public void RepeatTokens(FrequencyCounter frequencyCounter)
+    {
+        var frequencyAnalysis = frequencyCounter.GetFrequencyAnalysis("abcabc");
+        frequencyAnalysis.TokenCount.Should().Equal(new Dictionary<char, int>
+        {
+            ['a'] = 2,
+            ['b'] = 2,
+            ['c'] = 2,
+        });
+        frequencyAnalysis.TokensByFrequency.Should().Equal('a', 'b', 'c');
+    }
+
+    [Theory(DisplayName = "FrequencyCounter: Sort Tokens by Frequency"), AutoMoq]
+    public void SortByFrequency(FrequencyCounter frequencyCounter)
+    {
+        const string cipherText = "abccac";
+
+        var frequencyAnalysis = frequencyCounter.GetFrequencyAnalysis(cipherText);
+
+        frequencyAnalysis.TokenCount.Should().Equal(new Dictionary<char, int>
+        {
+            ['a'] = 2,
+            ['b'] = 1,
+            ['c'] = 3,
+        });
+        frequencyAnalysis.TokensByFrequency.Should().Equal('c', 'a', 'b');
+    }
+
+    [Theory(DisplayName = "FrequencyCounter: The Raven"), AutoMoq]
+    public void TheRaven(FrequencyCounter frequencyCounter)
+    {
+        const string cipherText = @"
+Once upon a midnight dreary, while I pondered, weak and weary,
+Over many a quaint and curious volume of forgotten lore—
+    While I nodded, nearly napping, suddenly there came a tapping,
+As of some one gently rapping, rapping at my chamber door.
+’Tis some visitor, I muttered, tapping at my chamber door—
+    Only this and nothing more.";
+
+        var frequencyAnalysis = frequencyCounter.GetFrequencyAnalysis(cipherText);
+
+        frequencyAnalysis.TokensByFrequency.Take(6)
+                                            .Should()
+                                            .Equal(' ', 'e', 'n', 'a', 'o', 'r');
     }
 }
--- IntBasis.Fun.Cryptography/FrequencyCounter.cs
+++ IntBasis.Fun.Cryptography/FrequencyCounter.cs
@@ -1,10 +1,37 @@
 ﻿namespace IntBasis.Fun.Cryptography;
 
+public class FrequencyAnalysis
+{
+    public IDictionary<char, int> TokenCount { get; }
+    public IList<char> TokensByFrequency { get; }
+
+    public FrequencyAnalysis(IDictionary<char, int> tokenCount, IList<char> tokensByFrequency)
+    {
+        TokenCount = tokenCount ?? throw new ArgumentNullException(nameof(tokenCount));
+        TokensByFrequency = tokensByFrequency ?? throw new ArgumentNullException(nameof(tokensByFrequency));
+    }
+}
+
 /// <summary>
-/// Performs basic Frequency Analysis on ciphertext by counting symbols.
+/// Performs basic Frequency Analysis on ciphertext by counting tokens.
 /// <see href="https://en.wikipedia.org/wiki/Frequency_analysis"/>
 /// </summary>
 public class FrequencyCounter
 {
-
+    /// <inheritdoc/>
+    public FrequencyAnalysis GetFrequencyAnalysis(string cipherText)
+    {
+        var tokenCount = new Dictionary<char, int>();
+        foreach (char token in cipherText)
+        {
+            if (!tokenCount.ContainsKey(token))
+                tokenCount[token] = 1;
+            else
+                tokenCount[token]++;
+        }
+        var tokensByFrequency = tokenCount.OrderByDescending(kv => kv.Value)
+                                            .Select(kv => kv.Key)
+                                            .ToList();
+        return new FrequencyAnalysis(tokenCount, tokensByFrequency);
+    }
 }

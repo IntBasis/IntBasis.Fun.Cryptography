3a6d5f05ce5affb3f7b981a590206841ecc59a82
Jacob Foshee <jacobf@gmail.com>
2022-06-05 16:31:12 -0500

    test(sub): Add Gold Bug example

--- IntBasis.Fun.Cryptography.Tests/SubstitutionCipherServiceTest.cs
+++ IntBasis.Fun.Cryptography.Tests/SubstitutionCipherServiceTest.cs
@@ -1,5 +1,7 @@
 ﻿using FluentAssertions;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace IntBasis.Fun.Cryptography.Tests;
@@ -59,5 +61,55 @@ public class SubstitutionCipherServiceTest
         var output = subject.ApplyCharacterSubstitution("c.b.a!", mapping, options);
 
         output.Should().Be("z?y?x?");
+    }
+
+    [Theory(DisplayName = "Substitution: The Gold Bug"), Integration]
+    public void TheGoldBug(SubstitutionCipherService substitutionCipherService, IFrequencyCounter frequencyCounter)
+    {
+        var path = Path.Combine("TestData", "GoldBug.txt");
+        var cipherText = File.ReadAllText(path);
+        // HACK: Remove new-lines
+        cipherText = cipherText.ReplaceLineEndings("");
+        var options = new FrequencyAnalysisOptions { IgnoreWhitespace = true };
+        var frequencyAnalysis = frequencyCounter.GetFrequencyAnalysis(cipherText, options);
+        // Now, in English, the letter which most frequently occurs is e.
+        var e = frequencyAnalysis.TokensByFrequency.First();
+        // Let us assume 8,then, as 'e'.
+        e.Should().Be('8');
+        // Now, of all the words in the language, 'the' is the most usual;
+        // let us see, therefore, whether there are not repetitions of any three characters ...
+        // the last of them being 8 ...
+        // they will most probably represent the word 'the'
+        var the = frequencyAnalysis.TrigramsByFrequency.First(trigram => trigram.EndsWith(e));
+        Dictionary<char, char> mapping = new()
+        {
+            [the[0]] = 't',
+            [the[1]] = 'h',
+            [the[2]] = 'e'
+        };
+
+        var output = substitutionCipherService.ApplyCharacterSubstitution(cipherText, mapping, default);
+        output.Should().Contain("t-eeth");
+        // TODO: Going through the alphabet ... we arrive at the word 'tree' as the sole possible reading
+        var r_i = output.IndexOf("t-eeth") + 1;
+        var r = cipherText[r_i];
+        mapping[r] = 'r';
+
+        output = substitutionCipherService.ApplyCharacterSubstitution(cipherText, mapping, default);
+        output.Should().Contain("thetreethr---h");
+        // ...when the word 'through' makes itself evient at once.
+        // But this discovery gives us three new letter 'o', 'u', and 'g'
+        var o_i = output.IndexOf("thetreethr---h") + 10;
+        var u_i = o_i + 1;
+        var g_i = u_i + 1;
+        var o = cipherText[o_i];
+        var u = cipherText[u_i];
+        var g = cipherText[g_i];
+        mapping[o] = 'o';
+        mapping[u] = 'u';
+        mapping[g] = 'g';
+
+        output = substitutionCipherService.ApplyCharacterSubstitution(cipherText, mapping, default);
+        output.Should().Contain("thetreethrough");
     }
 }

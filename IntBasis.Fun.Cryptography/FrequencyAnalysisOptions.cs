0f3997b818a45ee3fb4f908423126a8c7c983cdf
Jacob Foshee <jacobf@gmail.com>
2022-06-05 15:29:14 -0500

    refactor(freq): Extract classes to files

---
+++ IntBasis.Fun.Cryptography/FrequencyAnalysisOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace IntBasis.Fun.Cryptography;
+
+public class FrequencyAnalysisOptions
+{
+    /// <summary>
+    /// If true, spaces, new-lines and other whitespace characters will not contribute to frequency analysis
+    /// </summary>
+    public bool IgnoreWhitespace { get; set; }
+
+    public static FrequencyAnalysisOptions Default => new()
+    {
+        IgnoreWhitespace = false
+        // TODO: IgnoreCase
+    };
+}

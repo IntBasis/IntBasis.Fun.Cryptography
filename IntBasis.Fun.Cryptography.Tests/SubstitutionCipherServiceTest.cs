77528a0bfcd9a379edcb061d0f376daf6e6a88bf
Jacob Foshee <jacobf@gmail.com>
2022-06-05 15:18:51 -0500

    feat(sub): Start `SubstitutionCipherService`

---
+++ IntBasis.Fun.Cryptography.Tests/SubstitutionCipherServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace IntBasis.Fun.Cryptography.Tests;
+
+public class SubstitutionCipherServiceTest
+{
+    [Fact(DisplayName = "Substitution: Empty")]
+    public void Empty()
+    {
+        SubstitutionCipherService subject = new(new CharacterEncoderService());
+        Dictionary<char, char> mapping = new();
+
+        var output = subject.ApplyCharacterSubstitution("", mapping);
+
+        output.Should().BeEmpty();
+    }
+
+    [Fact(DisplayName = "Substitution: One Matching Character")]
+    public void OneMatchingChar()
+    {
+        SubstitutionCipherService subject = new(new CharacterEncoderService());
+        Dictionary<char, char> mapping = new()
+        {
+            ['a'] = 'x'
+        };
+
+        var output = subject.ApplyCharacterSubstitution("aaa", mapping);
+
+        output.Should().Be("xxx");
+    }
+}
---
+++ IntBasis.Fun.Cryptography/SubstitutionCipherService.cs
@@ -0,0 +1,19 @@
+﻿namespace IntBasis.Fun.Cryptography;
+
+/// <summary>
+/// Substitutes one token for another to produce clear text or cypher text
+/// </summary>
+public class SubstitutionCipherService
+{
+    private readonly ICharacterEncoderService characterEncoderService;
+
+    public SubstitutionCipherService(ICharacterEncoderService characterEncoderService)
+    {
+        this.characterEncoderService = characterEncoderService ?? throw new ArgumentNullException(nameof(characterEncoderService));
+    }
+
+    public string ApplyCharacterSubstitution(string inputText, IDictionary<char, char> substitutionMapping)
+    {
+        return characterEncoderService.Encode(inputText, c => substitutionMapping[c]);
+    }
+}
